c12a68d0dc7947ddbd6f009bc4de9a94ceefbe17
NeurixIT <neurixit@gmail.com>
2025-02-14 10:19:11 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    toggleButton.addEventListener('click', function () {
+      document.documentElement.classList.toggle('dark');
+    });
+  });
+</script>
+
